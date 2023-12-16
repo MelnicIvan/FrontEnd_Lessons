048085e3ff95dd62386f9f88fc241f65b6d523c5
MelnicIvan <144802494+MelnicIvan@users.noreply.github.com>
2023-12-16 11:05:56 +0200

    Add files via upload

---
+++ HomeworkJS(№1)/script.js
@@ -0,0 +1,59 @@
+console.log('Ivan');
+
+console.log(3);
+
+console.log('Добро ' + 'пожаловать ' + 'на курс');
+
+//alert(2019);
+
+console.log(2019-200);
+
+document.getElementById('six').innerHTML = 'Hello World';
+
+document.getElementById('seven').innerHTML = 12 * 12;
+
+const div8 = document.querySelector('.out-8');
+div8.textContent = 'task-8';
+
+document.querySelector('.out-10').innerHTML='<h2>Hi</h2>';
+
+const out11 = document.querySelector('.out-11');
+out11.innerHTML = '123';
+
+out11.innerHTML += '456';
+
+const a = document.querySelector('.out-12');
+a.innerHTML = 3.1415;
+
+let out13 = document.querySelector('.out-13');
+out13.innerHTML = ' "<img src="https://static.wikia.nocookie.net/rustarwars/images/d/d6/Yoda_SWSB.png/revision/latest?cb=20171222112613" alt="js"> " ';
+
+let z1=6, z2=3;
+document.querySelector('.out-14').innerHTML=z1*z2;
+
+let y1=6, y2=3;
+document.querySelector('.out-15').innerHTML=z1/z2;
+
+let x1='Hello', x2=5;
+document.querySelector('.out-16').innerHTML= x1 + x2;
+
+const out17 = document.querySelector('.out-17');
+console.log(out17);
+
+let out18 = document.querySelector('.out-18');
+console.log(out18);
+out18 = 5;
+console.log(out18);
+
+let out19 = document.querySelector('.out-19-test');
+console.log(out19);
+
+let out20 = document.querySelector('.out-20');
+out20.textContent = '<h2>Hi</h2>';
+
+
+
+
+
+
+
